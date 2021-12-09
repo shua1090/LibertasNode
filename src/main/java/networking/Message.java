8e4635d01553c65280f033e37d0268f102f903eb
shua1090 <shynn.lawrence@gmail.com>
2021-12-08 19:37:56 -0800

    Compiled dependency directly

--- src/main/java/networking/Message.java
+++ src/main/java/networking/Message.java
@@ -1,12 +1,16 @@
+package networking;
+
+import com.offbynull.kademlia.*;
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 public class Message {
-    public final MessageSendType sendType;
-    public final MessageDataType dataType;
-    final long MessageLength;
-    public final byte[] data;
+    private MessageSendType sendType = null;
+    private MessageDataType dataType = null;
+    private int MessageLength = 0;
+    private byte[] data = new byte[0];
 
-    private Message(){};
+    private Message(){}
 
     public Message(MessageSendType sendType, MessageDataType dataType, byte[] data){
         this.sendType = sendType;
@@ -16,31 +20,31 @@ public class Message {
     }
 
     public long deserializeMessageLength(byte[] headerData){
-        byte[] messLen = new byte[8];
+        byte[] messLen = new byte[4];
         for (int i = 2; i < headerData.length; i++){
             messLen[i-2] = headerData[i];
         }
         return ByteUtils.bytesToLong(messLen);
     }
 
-    public Message deserializeData(byte[] data){
+    public static Message deserializeData(byte[] data) throws InvalidMessageType {
         Message m = new Message();
         m.sendType = MessageSendType.deserialize(data[0]);
         m.dataType = MessageDataType.deserialize(data[1]);
-        byte[] messagelen = new byte[8];
+        byte[] messagelen = new byte[4];
         for (int i = 0; i < messagelen.length; i++){
             messagelen[i] = data[i+2];
         }
         m.MessageLength = ByteUtils.bytesToLong(messagelen);
         for (int i = 0; i < m.MessageLength; i++){
-            m.data[i] = data[10+i];
+            m.data[i] = data[6+i];
         }
         
         return m;
     }
 
     public byte[] serializeData(){
-        byte[] serializedData = new byte[10+MessageLength]
+        byte[] serializedData = new byte[6+MessageLength];
         serializedData[0] = sendType.serialize();
         serializedData[1] = dataType.serialize();
 
@@ -50,25 +54,47 @@ public class Message {
         }
 
         for (int i = 0; i < MessageLength; i++){
-            serializedData[10+i] = data[i];
+            serializedData[6+i] = data[i];
         }
 
         return serializedData;
     }
 
-    class ByteUtils {
-        private static ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);    
+    public static void main(String[] args) {
+        System.out.println("yes");
+        Message m = new Message(MessageSendType.REQUEST, MessageDataType.EPOCH_NUM, new byte[0]);
+        System.out.println(m);
+        System.out.println(Arrays.toString(m.serializeData()));
+        try {
+            System.out.println(Message.deserializeData(m.serializeData()));
+        } catch (InvalidMessageType e) {
+            e.printStackTrace();
+        }
+//        Message m = new Message()
+    }
+
+    static class ByteUtils {
+        private static final ByteBuffer buffer = ByteBuffer.allocate(4);
     
-        public static byte[] longToBytes(long x) {
-            buffer.putLong(0, x);
+        public static byte[] longToBytes(int x) {
+            buffer.putInt(0, x);
             return buffer.array();
         }
     
-        public static long bytesToLong(byte[] bytes) {
+        public static int bytesToLong(byte[] bytes) {
             buffer.put(bytes, 0, bytes.length);
             buffer.flip();//need flip 
-            return buffer.getLong();
+            return buffer.getInt();
         }
     }
 
+    @Override
+    public String toString() {
+        return "Message{" +
+                "sendType=" + sendType +
+                ", dataType=" + dataType +
+                ", MessageLength=" + MessageLength +
+                ", data=" + Arrays.toString(data) +
+                '}';
+    }
 }
--- src/main/java/networking/MessageTypes.java
+++ src/main/java/networking/MessageTypes.java
@@ -1,17 +1,18 @@
+package networking;
 
 class InvalidMessageType extends Exception{}
 
-public enum MessageDataType {
+enum MessageDataType {
 
     // Blocks
-    REQUEST_BLOCK(128), // Do you have this block? 
+    REQUEST_BLOCK(64), // Do you have this block?
     //Add delay so that a negligent node can't request someone's block for "HAS_BLOCK"
      // The data after this contains the block
-    BLOCK_PROOF(129), // Prove to me that you have this block
+    BLOCK_PROOF(65), // Prove to me that you have this block
      // Submit Hash as proof
 
     // Transactions
-    SUBMIT_TRANSACTION(255),
+    SUBMIT_TRANSACTION(66),
     
     // Search
     PING_PONG(0),
@@ -21,57 +22,46 @@ public enum MessageDataType {
     EPOCH_NUM(2),
     BLOCK_NUM(3),
     REQUEST_DIFFICULTY(4),
-    REQUEST_MERKLE(5),
+    REQUEST_MERKLE(5);
 
-    private byte id;
-    MessageDataType(byte id){
-        this.id = id;
+    private final byte id;
+    MessageDataType(int id){
+        this.id = (byte) id;
     }
     public byte serialize(){
         return this.id;
     }
-    public static MessageDataType deserialize(byte inputData) {
-        switch (inputData) {
-            case 0: return PING_PONG;
-            case 1: return FIND_NODE;
-            case 2:
-                return EPOCH_NUM;
-            case 3:
-                return BLOCK_NUM;
-            case 4:
-                return REQUEST_DIFFICULTY;
-            case 5:
-                return REQUEST_MERKLE;
-            case 128:
-                return REQUEST_BLOCK;
-            case 129:
-                return BLOCK_PROOF;
-            case 255:
-                return SUBMIT_TRANSACTION;
-            default:
-                throw InvalidMessageType();
-                return null;
-        }
+    public static MessageDataType deserialize(byte inputData) throws InvalidMessageType {
+        return switch (inputData) {
+            case 0 -> PING_PONG;
+            case 1 -> FIND_NODE;
+            case 2 -> EPOCH_NUM;
+            case 3 -> BLOCK_NUM;
+            case 4 -> REQUEST_DIFFICULTY;
+            case 5 -> REQUEST_MERKLE;
+            case 64 -> REQUEST_BLOCK;
+            case 65 -> BLOCK_PROOF;
+            case 66 -> SUBMIT_TRANSACTION;
+            default -> throw new InvalidMessageType();
+        };
     }
 }
 
 enum MessageSendType {
     REQUEST(0),
     SUBMIT(1);
-    private byte id;
-    MessageSendType(byte id){
-        this.id = id;
+    private final byte id;
+    MessageSendType(int id){
+        this.id = (byte) id;
     }
     public byte serialize(){
         return this.id;
     }
-    public static MessageSendType deserialize(byte inputData) {
-        switch (inputData) {
-            case 0: return REQUEST;
-            case 1: return SUBMIT;
-            default:
-                throw InvalidMessageType();
-                return null;
-        }
+    public static MessageSendType deserialize(byte inputData) throws InvalidMessageType {
+        return switch (inputData) {
+            case 0 -> REQUEST;
+            case 1 -> SUBMIT;
+            default -> throw new InvalidMessageType();
+        };
     }
 }
